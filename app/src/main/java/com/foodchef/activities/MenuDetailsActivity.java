2192d4dd16bacbd3aa95d76a120e3430177172bc
Binayak Adhikari <blazebnayak@gmail.com>
2018-09-29 19:23:47 +0545

    Initial design

---
+++ app/src/main/java/com/foodchef/activities/MenuDetailsActivity.java
@@ -0,0 +1,19 @@
+package com.foodchef.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.foodchef.R;
+
+/**
+ * Created by blaze on 09-29-2018.
+ */
+
+public class MenuDetailsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_details);
+    }
+}
